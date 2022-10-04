a9138e95c6a02779a140d9de3d5426b0fe960743
grezli <grezli@mail.ru>
2022-10-04 16:54:18 +0300

    lec 2

--- lectures/Lesson_2/lec_2.py
+++ lectures/Lesson_2/lec_2.py
@@ -107,28 +107,28 @@
 # print(a[-2])
 
 # t = ()
-# print(type(t)) # tuple
+# print(type(t))                              # tuple
 # t = (1,)
-# print(type(t)) # tuple
+# print(type(t))                              # tuple
 # t = (1)
-# print(type(t)) # int
+# print(type(t))                              # int
 # t = (28, 9, 1990)
-# print(type(t)) # tuple
+# print(type(t))                              # tuple
 # colors = ['red', 'green', 'blue']
-# print(colors) # ['red', 'green', 'blue']
+# print(colors)                               # ['red', 'green', 'blue']
 # t = tuple(colors)
-# print(t) # ('red', 'green', 'blue')
+# print(t)                                    # ('red', 'green', 'blue')
 
 
 # t = tuple(['red', 'green', 'blue'])
-# print(t[0]) # red
-# print(t[2]) # blue
-# # print(t[10]) # IndexError: tuple index out of range
-# print(t[-2]) # green
-# # print(t[-200]) # IndexError: tuple index out of range
+# print(t[0])                                 # red
+# print(t[2])                                 # blue
+# # print(t[10])                              # IndexError: tuple index out of range
+# print(t[-2])                                # green
+# # print(t[-200])                            # IndexError: tuple index out of range
 # for e in t:
-#  print(e) # red green blue
-# t[0] = 'black' # TypeError: 'tuple' object does not support
+#  print(e)                                   # red green blue
+# t[0] = 'black'                              # TypeError: 'tuple' object does not support
 # item assignment
 
 
@@ -197,18 +197,18 @@
 
 # colors = {'red', 'green', 'blue'}
 
-# print(colors) # {'red', 'green', 'blue'}
+# print(colors)                 # {'red', 'green', 'blue'}
 # colors.add('red')
-# print(colors) # {'red', 'green', 'blue'}
+# print(colors)                 # {'red', 'green', 'blue'}
 # colors.add('gray')
-# print(colors) # {'red', 'green', 'blue','gray'}
+# print(colors)                 # {'red', 'green', 'blue','gray'}
 # colors.remove('red')
-# print(colors) # {'green', 'blue','gray'}
-# # colors.remove('red') # KeyError: 'red'
-# colors.discard('red') # ok
-# print(colors) # {'green', 'blue','gray'}
-# colors.clear() # { }
-# print(colors) # set()
+# print(colors)                 # {'green', 'blue','gray'}
+# # colors.remove('red')        # KeyError: 'red'
+# colors.discard('red')         # ok
+# print(colors)                 # {'green', 'blue','gray'}
+# colors.clear()                # { }
+# print(colors)                 # set()
 
 
 
