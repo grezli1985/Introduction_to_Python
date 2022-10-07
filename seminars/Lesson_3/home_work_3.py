4958dd6d6d02c5b89d388c47f3d94ab37b1b4697
grezli <grezli@mail.ru>
2022-10-07 17:29:17 +0300

    add home work 3

--- seminars/Lesson_3/home_work_3.py
+++ seminars/Lesson_3/home_work_3.py
@@ -105,20 +105,15 @@
 
 # fib = [0, 1]
 
+# for n in range(10):
+#     fib.append(fib[-2] - fib[-1])
+
+# fib.reverse()
+
 # for i in range(10):
 #     fib.append(fib[-1] + fib[-2])
 
-# # print(fib)
-
-
-# neg = [0, 1]
-
-# for n in range(10):
-#     neg.append(neg[-2] - neg[-1])
-
-# neg.reverse()
-
-# print(neg + fib[1:])
+# print(fib)
 
 
 
