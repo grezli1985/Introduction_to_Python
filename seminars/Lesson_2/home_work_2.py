b6ed70d2d1d86b9ca3002007f1d41c7ea013e1f2
grezli <grezli@mail.ru>
2022-10-14 18:07:40 +0300

    add home work 2

--- seminars/Lesson_2/home_work_2.py
+++ seminars/Lesson_2/home_work_2.py
@@ -88,5 +88,30 @@
 ''' Задание 5
         Реализуйте алгоритм перемешивания списка.   '''
 
+# ------------< Решения 1 >-------------
+
+# import random
+# lst = [random.randint(0,10) for i in range (random.randint(5,20))]
+# print(f"исходный список:\n {lst}")
+# random.shuffle(lst)
+# print(f"список после перемешивания:\n{lst}")
 
 
+# ------------< Решения 2 >-------------
+
+
+# from random import randint
+ 
+# n = int(input('Введите размер списка: '))
+# a = []
+# for i in range(n):
+#     a.append(randint(1, 150))
+# print(f'Первоначальный список {a}')
+ 
+ 
+# for i in range(n-1):
+#     for j in range(n-i-1):
+#         if a[j] > a[j+1]:
+#             a[j], a[j+1] = a[j+1], a[j]
+ 
+# print(f'Отсортированный список методом пузырька {a}')
